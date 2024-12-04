98d12627891e75b69b7e6bf869e2b94de4936e4f
inix84 <inix84@mail.ru>
2024-12-04 23:06:44 +0500

    10 commit 4 пункта выполнила

---
+++ src/org/skypro/skyshop/product/FixPriceProduct.java
@@ -0,0 +1,19 @@
+package org.skypro.skyshop.product;
+
+public class FixPriceProduct extends Product {// класс для продуктов со фиксированной ценой
+    private static final int FIX_PRICE = 210;
+
+    public FixPriceProduct(String name) {
+        super(name);
+    }
+
+    @Override
+    public int getPrice() {
+        return FIX_PRICE; // некоторая фиксированная цена
+    }
+
+    @Override
+    public String toString() {
+        return getName() + ": " + getPrice();
+    }
+}
